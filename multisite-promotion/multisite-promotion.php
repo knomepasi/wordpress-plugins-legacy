0138ce91c1bc5681cd43796cd40808be5d179e3c
Pasi Lallinaho <pasi@shimmerproject.org>
2012-04-14 09:47:07 +0300

    Multisite Promotion: 1.0 Add widgets to link to other (multi)sites for promotion.

---
+++ multisite-promotion/multisite-promotion.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ *  Plugin Name: Multisite Promotion
+ *  Description: Enables adding widgets to link to other (multi)sites for promotion
+ *  Author: Pasi Lallinaho
+ *  Version: 1.0
+ *  Author URI: http://open.knome.fi/
+ *  Plugin URI: https://github.com/knomepasi/WordPress-plugins
+ *
+ */
+
+/*  Init plugin
+ *
+ */
+
+add_action( 'init', 'MultisitePromotionInit' );
+
+function MultisitePromotionInit( ) {
+	/* Load text domain for i18n */
+	load_plugin_textdomain( 'multisite-promotion' );
+}
+
+/*  Widget
+ *
+ */
+
+add_action( 'widgets_init', create_function( '', 'return register_widget("MultisitePromotionWidget");' ) );
+
+class MultisitePromotionWidget extends WP_Widget {
+	/** constructor */
+	function MultisitePromotionWidget() {
+		$ops = array( "description" => __( 'Enables adding widgets to link to other sites on multisites.', 'multisite-promotion' ) );
+		parent::WP_Widget( false, $name = __( 'Multisite Promotion', 'multisite-promotion' ), $ops );
+	}
+
+	/** @see WP_Widget::widget */
+	function widget( $args, $instance ) {
+		extract( $args );
+
+		$promote_blog_id = $instance['promote_blog_id'];
+
+		print $before_widget;
+		switch_to_blog( $promote_blog_id );
+		$promote_domain = parse_url( get_option( 'home' ) );
+
+		print '<div class="promote-blog host-' . str_replace( '.', '-', $promote_domain['host'] ) . '">';
+		print '<a href="' . get_bloginfo( 'url' ) . '">';
+			print $before_title . get_bloginfo( 'name' ) . $after_title;
+			print "<p>" . str_replace( "//", "", get_bloginfo( 'description' ) ) . "</p>";
+		print '</a></div>';
+
+		restore_current_blog( );
+		print $after_widget;
+	}
+
+	/** @see WP_Widget::update */
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['promote_blog_id'] = strip_tags( $new_instance['promote_blog_id'] );
+		return $instance;
+	}
+
+	/** @see WP_Widget::form */
+	function form( $instance ) {
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'promote_blog_id' ); ?>"><?php _e( 'Blog to promote', 'multisite-promotion' ); ?><br />
+				<select class="widefat" id="<?php echo $this->get_field_id( 'promote_blog_id' ); ?>" name="<?php echo $this->get_field_name( 'promote_blog_id' ); ?>">
+					<?php
+						global $wpdb;
+						$blogs = $wpdb->get_results( 'SELECT * FROM wp_knomefi_blogs WHERE public = 1 ORDER BY domain ASC', OBJECT );
+
+						foreach( $blogs as $blog ) {
+							if( $blog->blog_id == $instance['promote_blog_id'] ) { $is_selected = ' selected="selected " '; } else { unset( $is_selected ); }
+							switch_to_blog( $blog->blog_id );
+							print '<option value="' . $blog->blog_id . '"' . $is_selected . '>' . get_bloginfo( 'name' ) . '</option>';
+							restore_current_blog( );
+						}
+					?>
+				</select>
+			</label>
+		</p>
+		<?php 
+	}
+}
+
+?>
