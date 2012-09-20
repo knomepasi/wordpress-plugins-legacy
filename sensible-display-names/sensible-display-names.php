3973cad38882f922d1e26060d4fee12ad57144db
Pasi Lallinaho <pasi@shimmerproject.org>
2012-09-20 15:07:39 +0300

    Add Sensible Display Names 1.0-RC1

---
+++ sensible-display-names/sensible-display-names.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ *  Plugin Name: Sensible Display Names
+ *  Description: Sets users' display names to "First Last" on registration.
+ *  Author: Pasi Lallinaho
+ *  Version: 1.0-RC1
+ *  Author URI: http://open.knome.fi/
+ *  Plugin URI: https://github.com/knomepasi/WordPress-plugins
+ *
+ */
+
+add_action( 'user_register', 'sensible_names_register' );
+
+function sensible_names_register( $user_id ) {
+	$display = $_POST['first_name'] . " " . $_POST['last_name'];
+
+	global $wpdb;
+	$wpdb->update(
+		$wpdb->users,
+		array( "display_name" => $display ),
+		array( "ID" => $user_id ),
+		array( "%s" ),
+		array( "%d" )
+	);
+}
+
+?>
