14936c18525254891df4f093c820cd3efa026c9c
Pasi Lallinaho <pasi@shimmerproject.org>
2013-01-13 00:20:44 +0200

    Simple Stats 1.1

--- plugins/simple-stats/simple-stats.php
+++ plugins/simple-stats/simple-stats.php
@@ -1,9 +1,9 @@
 <?php
 /*
  *  Plugin Name: Simple Stats
- *  Description: Gather simple page/post load statistics.
+ *  Description: Simple load statistics for single or multisite.
  *  Author: Pasi Lallinaho
- *  Version: 1.0-RC
+ *  Version: 1.1
  *  Author URI: http://open.knome.fi/
  *  Plugin URI: https://github.com/knomepasi/WordPress-plugins
  *
@@ -13,16 +13,16 @@
  *
  */
 
-register_activation_hook( __FILE__, 'MultisiteStatsDBInit' );
+register_activation_hook( __FILE__, 'SimpleStatsDBInit' );
 
-function MultisiteStatsDBInit( ) {
+function SimpleStatsDBInit( ) {
 	global $wpdb;
-	$wpdb->multisitestats = $wpdb->base_prefix . "multisitestats";
+	$wpdb->simplestats = $wpdb->base_prefix . "simplestats";
 
 	if( !empty( $wpdb->charset ) ) { $charset_collate = "DEFAULT CHARACTER SET $wpdb->charset"; }
 	if( !empty( $wpdb->collate ) ) { $charset_collate .= " COLLATE $wpdb->collate"; }
 
-	$wp_query = "CREATE TABLE IF NOT EXISTS `" . $wpdb->multisitestats . "` (
+	$wp_query = "CREATE TABLE IF NOT EXISTS `" . $wpdb->simplestats . "` (
 			blog_id bigint(20) unsigned NOT NULL,
 			post_id bigint(20) unsigned NOT NULL,
 			month date NOT NULL,
@@ -36,41 +36,41 @@ function MultisiteStatsDBInit( ) {
  *
  */
 
-add_action( 'init', 'MultisiteStatsInit' );
+add_action( 'init', 'SimpleStatsInit' );
 
-function MultisiteStatsInit( ) {
+function SimpleStatsInit( ) {
 	/* Load text domain for i18n */
-	load_plugin_textdomain( 'multisite-stats' );
+	load_plugin_textdomain( 'simple-stats', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
 	global $wpdb;
-	$wpdb->multisitestats = $wpdb->base_prefix . "multisitestats";
+	$wpdb->simplestats = $wpdb->base_prefix . "simplestats";
 }
 
 /*  Add hook to all page loads
  *
  */
 
-add_action( 'shutdown', 'MultisiteStatsHit' );
+add_action( 'shutdown', 'SimpleStatsHit' );
 
-function MultisiteStatsHit( ) {
+function SimpleStatsHit( ) {
 	global $blog_id, $wpdb;
-	$wpdb->multisitestats = $wpdb->base_prefix . "multisitestats";
+	$wpdb->simplestats = $wpdb->base_prefix . "simplestats"; // FIXME: del?
 
 	$month = gmdate( "Y-m-00" );
 	$post_id = get_the_ID( );
 
 	if( $post_id == 0 || !is_single( ) ) { exit; }
 
-	$r = $wpdb->get_row( "SELECT count, month FROM $wpdb->multisitestats WHERE blog_id = '" . $blog_id . "' AND post_id = '" . $post_id . "' AND month = '" . $month . "'", ARRAY_A );
+	$r = $wpdb->get_row( "SELECT count, month FROM $wpdb->simplestats WHERE blog_id = '" . $blog_id . "' AND post_id = '" . $post_id . "' AND month = '" . $month . "'", ARRAY_A );
 	if( $r['count'] < 1 ) {
 		$wpdb->insert(
-			$wpdb->multisitestats,
+			$wpdb->simplestats,
 			array( "blog_id" => $blog_id, "post_id" => $post_id, "month" => $month, "count" => 1 )
 		);
 	} else {
 		$new_count = $r['count'] + 1;
 		$wpdb->update(
-			$wpdb->multisitestats,
+			$wpdb->simplestats,
 			array( "count" => $new_count ),
 			array( "blog_id" => $blog_id, "post_id" => $post_id, "month" => $month )
 		);
@@ -81,18 +81,18 @@ function MultisiteStatsHit( ) {
  *
  */
 
-add_action( 'admin_menu', 'MultisiteStatsMenu' );
+add_action( 'admin_menu', 'SimpleStatsMenu' );
 
-function MultisiteStatsMenu( ) {
+function SimpleStatsMenu( ) {
 	if( current_user_can( 'activate_plugins' ) ) {
-		$ms_stats = add_menu_page( __( 'Statistics', 'multisite-stats' ), 'Statistics', 'see_stats', 'multisite-stats', 'MultisiteStatsAdmin', null, 50 );
+		$ms_stats = add_menu_page( __( 'Statistics', 'simple-stats' ), __( 'Statistics', 'simple-stats' ), 'see_stats', 'simple-stats', 'SimpleStatsAdmin', null, 50 );
 	}
 }
 
-function MultisiteStatsAdmin( ) {
+function SimpleStatsAdmin( ) {
 	if( current_user_can( 'activate_plugins' ) ) {
 		print '<div class="wrap">';
-		print '<h2>' . __( 'Statistics', 'multisite-stats' ) . '</h2>';
+		print '<h2>' . __( 'Statistics', 'simple-stats' ) . '</h2>';
 
 		if( !$_POST['submit-month'] ) { unset( $_POST['month'] ); }
 		if( !$_POST['submit-year'] ) { unset( $_POST['year'] ); }
@@ -117,9 +117,9 @@ function _ms_stats_list( $month = null, $year = null ) {
 		$filter = $wpdb->prepare( " AND YEAR( month ) = '%d'", $year );
 	}
 
-	$totals = $wpdb->get_results( "SELECT post_id, SUM( count ) as total FROM $wpdb->multisitestats WHERE blog_id = '" . $blog_id . "'" . $filter .  " GROUP BY post_id ORDER BY total DESC", ARRAY_A );
+	$totals = $wpdb->get_results( "SELECT post_id, SUM( count ) as total FROM $wpdb->simplestats WHERE blog_id = '" . $blog_id . "'" . $filter .  " GROUP BY post_id ORDER BY total DESC", ARRAY_A );
 	if( is_array( $totals ) ) {
-		print '<table class="widefat"><thead><tr><th style="width: 80px;">' . __( 'Loads', 'multisite-stats' ) . '</th><th>' . __( 'Post', 'multisite-stats' ) . '</th></tr></thead><tbody>';
+		print '<table class="widefat"><thead><tr><th style="width: 80px;">' . __( 'Loads', 'simple-stats' ) . '</th><th>' . __( 'Post', 'simple-stats' ) . '</th></tr></thead><tbody>';
 		foreach( $totals as $item ) {
 			$cur_post = get_post( $item['post_id'] );
 			print "<tr><td>" . $item['total'] . "</td><td><a href='" . get_permalink( $cur_post->ID ) . "'>" . $cur_post->post_title . "</a></td></tr>";
@@ -131,13 +131,13 @@ function _ms_stats_list( $month = null, $year = null ) {
 function _ms_stats_months( ) {
 	global $blog_id, $wpdb;
 
-	$months = $wpdb->get_results( "SELECT DATE_FORMAT( month, '%m' ) as m_month, DATE_FORMAT( month, '%Y' ) as m_year, month FROM {$wpdb->multisitestats} WHERE blog_id = '" . $blog_id . "' GROUP BY month ORDER BY month DESC", ARRAY_A );
+	$months = $wpdb->get_results( "SELECT DATE_FORMAT( month, '%m' ) as m_month, DATE_FORMAT( month, '%Y' ) as m_year, month FROM {$wpdb->simplestats} WHERE blog_id = '" . $blog_id . "' GROUP BY month ORDER BY month DESC", ARRAY_A );
 	if( is_array( $months ) ) {
 		$select_y = '<select name="year" class="postform">';
-		$select_y .= '<option value="" ' . selected( $_POST['year'], false, false ) . '>' . __( 'Select year', 'multisite-stats' ) . '</option>';
+		$select_y .= '<option value="" ' . selected( $_POST['year'], false, false ) . '>' . __( 'Select year', 'simple-stats' ) . '</option>';
 
 		$select_m = '<select name="month" class="postform">';
-		$select_m .= '<option value="" ' . selected( $_POST['month'], false, false ) . '>' . __( 'Select month', 'multisite-stats' ) . '</option>';
+		$select_m .= '<option value="" ' . selected( $_POST['month'], false, false ) . '>' . __( 'Select month', 'simple-stats' ) . '</option>';
 		foreach( $months as $month ) {
 			if( $last_year != $month['m_year'] ) {
 				$select_y .= '<option value="' . $month['m_year'] . '" ' . selected( $_POST['year'], $month['m_year'], false ) . ' >' . $month['m_year'] . '</option>';
@@ -149,14 +149,14 @@ function _ms_stats_months( ) {
 		$select_y .= '</select> ';
 		$select_m .= '</select> ';
 
-		print '<form action="admin.php?page=multisite-stats" id="posts-filter" method="post">';
+		print '<form action="admin.php?page=simple-stats" id="posts-filter" method="post">';
 		print '<div class="tablenav actions">';
 
 		print $select_y;
-		print '<input type="submit" name="submit-year" id="post-query-submit" class="button-secondary" value="' . __( 'Filter', 'multisite-stats' ) . '" />';
+		print '<input type="submit" name="submit-year" id="post-query-submit" class="button-secondary" value="' . __( 'Filter', 'simple-stats' ) . '" />';
 
 		print $select_m;
-		print '<input type="submit" name="submit-month" id="post-query-submit" class="button-secondary" value="' . __( 'Filter', 'multisite-stats' ) . '" />';
+		print '<input type="submit" name="submit-month" id="post-query-submit" class="button-secondary" value="' . __( 'Filter', 'simple-stats' ) . '" />';
 
 		print '</div>';
 		print '</form>';
