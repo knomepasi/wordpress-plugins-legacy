08c726893f36b9b83bc032043d831b6bde37a727
Pasi Lallinaho <pasi@shimmerproject.org>
2013-05-05 18:10:21 +0300

    Separate Page Attachments: Enable changing the upload directory via the page_attachments_upload_dir option.

--- plugins/separate-page-attachments/separate-page-attachments.php
+++ plugins/separate-page-attachments/separate-page-attachments.php
@@ -9,17 +9,32 @@
  *
  */
 
-add_filter( 'upload_dir', 'page_upload_dir' );
+/*  On plugin activation, create options for default values if needed
+ *
+ */
 
-function page_upload_dir( $pathdata ) {
+register_activation_hook( __FILE__, 'SeparatePageAttachmentsActivate' );
+
+function SeparatePageAttachmentsActivate( ) {
+	// FIXME: Document
+	add_option( 'page_attachments_upload_dir', 'pages' );
+}
+
+/*  Add filter to uploads
+ *
+ */
+
+add_filter( 'upload_dir', 'SeparatePageAttachmentsUploadDir' );
+
+function SeparatePageAttachmentsUploadDir( $pathdata ) {
 	global $post;
 
 	$parent = get_post( $post->post_parent );
 
 	if( $parent->post_type = 'page' ) {
-		$pathdata['subdir'] = "/pages";
-		$pathdata['path'] = $pathdata['basedir'] . $pathdata['subdir'];
-		$pathdata['url'] = $pathdata['baseurl'] . $pathdata['subdir'];
+		$pathdata['subdir'] = "pages";
+		$pathdata['path'] = $pathdata['basedir'] . '/' . $pathdata['subdir'];
+		$pathdata['url'] = $pathdata['baseurl'] . '/' . $pathdata['subdir'];
 	}
 
 	return $pathdata;
