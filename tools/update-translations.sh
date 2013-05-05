377cb1ebccf90045a0f9dbab1eac8bb044f4bbf3
Pasi Lallinaho <pasi@shimmerproject.org>
2013-05-05 22:37:03 +0300

    Update the translation updating script to only update/create .pot files for plugins which have a languages subdirectory.

--- tools/update-translations.sh
+++ tools/update-translations.sh
@@ -4,11 +4,8 @@ PLUGIN_DIR="../plugins/*"
 
 for DIR in $PLUGIN_DIR
 do
-	if [ -d $DIR ]; then
+	if [ -d "$DIR" ] && [ -d "$DIR/languages/" ]; then
 		PLUGIN_NAME=$(basename $DIR)
-		if [ ! -d $DIR/languages/ ]; then
-			mkdir $DIR/languages/
-		fi
 		xgettext -o $DIR/languages/$PLUGIN_NAME.pot -L PHP -k__ -k_e -k_n:1,2 -k_x:1,2c -k_ex:1,2c -k_nx:4c,1,2 -kesc_attr_-kesc_attr_e -kesc_attr_x:1,2c -kesc_html__ -kesc_html_e -kesc_html_x:1,2c -k_n_noop:1,2 -k_nx_noop:4c,1,2 $DIR/*.php
 	fi
 done
