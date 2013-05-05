35211ec75580ebdbb3d064c9d665fc6080083a2f
Pasi Lallinaho <pasi@shimmerproject.org>
2013-05-05 18:24:08 +0300

    Add Clean Head 1.0.

---
+++ plugins/clean-head/clean-head.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ *  Plugin Name: Clean Head
+ *  Description: Remove unnecessary information from &lt;head&gt;.
+ *  Author: Pasi Lallinaho
+ *  Version: 1.0
+ *  Author URI: http://open.knome.fi/
+ *  Plugin URI: http://wordpress.knome.fi/
+ *
+ */
+
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+remove_action( 'wp_head', 'feed_links', 2 );
+remove_action( 'wp_head', 'rsd_link' );
+remove_action( 'wp_head', 'wlwmanifest_link' );
+remove_action( 'wp_head', 'index_rel_link' );
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+remove_action( 'wp_head', 'wp_generator' );
+
+?>
