118a25e6903a3cf80dec22a41a4319f0ef56f24d
Pasi Lallinaho <pasi@shimmerproject.org>
2013-05-12 00:15:41 +0300

    Simple Stats: Update translations.

--- tools/update-translations.sh
+++ tools/update-translations.sh
@@ -5,7 +5,7 @@ PLUGIN_DIR="../plugins"
 if [ -n "$1" ]; then
 	for PLUGIN in $@
 	do
-		echo xgettext -o $PLUGIN_DIR/$PLUGIN/languages/$PLUGIN.pot -L PHP -k__ -k_e -k_n:1,2 -k_x:1,2c -k_ex:1,2c -k_nx:4c,1,2 -kesc_attr_-kesc_attr_e -kesc_attr_x:1,2c -kesc_html__ -kesc_html_e -kesc_html_x:1,2c -k_n_noop:1,2 -k_nx_noop:4c,1,2 $PLUGIN_DIR/$PLUGIN/*.php
+		xgettext -o $PLUGIN_DIR/$PLUGIN/languages/$PLUGIN.pot -L PHP -k__ -k_e -k_n:1,2 -k_x:1,2c -k_ex:1,2c -k_nx:4c,1,2 -kesc_attr_-kesc_attr_e -kesc_attr_x:1,2c -kesc_html__ -kesc_html_e -kesc_html_x:1,2c -k_n_noop:1,2 -k_nx_noop:4c,1,2 $PLUGIN_DIR/$PLUGIN/*.php
 	done
 else
 	PLUGIN_DIR="$PLUGIN_DIR/*"
@@ -13,7 +13,7 @@ else
 	do
 		if [ -d "$DIR" ] && [ -d "$DIR/languages/" ]; then
 			PLUGIN_NAME=$(basename $DIR)
-			echo xgettext -o $DIR/languages/$PLUGIN_NAME.pot -L PHP -k__ -k_e -k_n:1,2 -k_x:1,2c -k_ex:1,2c -k_nx:4c,1,2 -kesc_attr_-kesc_attr_e -kesc_attr_x:1,2c -kesc_html__ -kesc_html_e -kesc_html_x:1,2c -k_n_noop:1,2 -k_nx_noop:4c,1,2 $DIR/*.php
+			xgettext -o $DIR/languages/$PLUGIN_NAME.pot -L PHP -k__ -k_e -k_n:1,2 -k_x:1,2c -k_ex:1,2c -k_nx:4c,1,2 -kesc_attr_-kesc_attr_e -kesc_attr_x:1,2c -kesc_html__ -kesc_html_e -kesc_html_x:1,2c -k_n_noop:1,2 -k_nx_noop:4c,1,2 $DIR/*.php
 		fi
 	done
 fi
