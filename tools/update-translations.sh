328ab50baac281e7ef6626d7afc1dceba0af0b6d
Pasi Lallinaho <pasi@shimmerproject.org>
2013-05-11 22:28:08 +0300

    Update the update-translations script to take plugins as arguments; if arguments exist, translations will only be updated for those plugins

--- tools/update-translations.sh
+++ tools/update-translations.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-PLUGIN_DIR="../plugins/*"
+PLUGIN_DIR="../plugins"
 
-for DIR in $PLUGIN_DIR
-do
-	if [ -d "$DIR" ] && [ -d "$DIR/languages/" ]; then
-		PLUGIN_NAME=$(basename $DIR)
-		xgettext -o $DIR/languages/$PLUGIN_NAME.pot -L PHP -k__ -k_e -k_n:1,2 -k_x:1,2c -k_ex:1,2c -k_nx:4c,1,2 -kesc_attr_-kesc_attr_e -kesc_attr_x:1,2c -kesc_html__ -kesc_html_e -kesc_html_x:1,2c -k_n_noop:1,2 -k_nx_noop:4c,1,2 $DIR/*.php
-	fi
-done
+if [ -n "$1" ]; then
+	for PLUGIN in $@
+	do
+		echo xgettext -o $PLUGIN_DIR/$PLUGIN/languages/$PLUGIN.pot -L PHP -k__ -k_e -k_n:1,2 -k_x:1,2c -k_ex:1,2c -k_nx:4c,1,2 -kesc_attr_-kesc_attr_e -kesc_attr_x:1,2c -kesc_html__ -kesc_html_e -kesc_html_x:1,2c -k_n_noop:1,2 -k_nx_noop:4c,1,2 $PLUGIN_DIR/$PLUGIN/*.php
+	done
+else
+	PLUGIN_DIR="$PLUGIN_DIR/*"
+	for DIR in $PLUGIN_DIR
+	do
+		if [ -d "$DIR" ] && [ -d "$DIR/languages/" ]; then
+			PLUGIN_NAME=$(basename $DIR)
+			echo xgettext -o $DIR/languages/$PLUGIN_NAME.pot -L PHP -k__ -k_e -k_n:1,2 -k_x:1,2c -k_ex:1,2c -k_nx:4c,1,2 -kesc_attr_-kesc_attr_e -kesc_attr_x:1,2c -kesc_html__ -kesc_html_e -kesc_html_x:1,2c -k_n_noop:1,2 -k_nx_noop:4c,1,2 $DIR/*.php
+		fi
+	done
+fi
+
+
