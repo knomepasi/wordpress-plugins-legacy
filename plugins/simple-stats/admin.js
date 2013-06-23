af356a38a8e92b75379f1a02f9f557dd3992a70e
Pasi Lallinaho <pasi@shimmerproject.org>
2013-06-24 02:37:28 +0300

    Remove Simple Stats for now since it's buggy.

--- plugins/simple-stats/admin.js
+++
@@ -1,8 +0,0 @@
-jQuery( function( ) {
-	jQuery( '.show-more a' ).click( function( e ) {
-
-		jQuery( '.more' ).show( 'blind' );
-		jQuery( '.show-more' ).hide( );
-		e.preventDefault( );
-	} );
-} );
