5d5e51707aa3ab49689808d823a01950c11fbd29
Pasi Lallinaho <pasi@shimmerproject.org>
2016-10-13 19:42:04 +0300

    Compact & Chronological 1.1.1 - Improve description. - Consistent function naming. - Update the translation template and the Finnish translation. - Use checked(). - Use echo instead of print.

--- plugins/compact-chronological/compact-chronological.php
+++ plugins/compact-chronological/compact-chronological.php
@@ -1,68 +1,71 @@
 <?php
 /*
  *  Plugin Name: Compact & Chronological
- *  Description: Show month archive links in a compact view.
+ *  Description: Show your monthly archive links in a compact view.
  *  Author: Pasi Lallinaho
- *  Version: 1.1
+ *  Version: 1.1.1
  *  Author URI: http://open.knome.fi/
  *  Plugin URI: http://wordpress.knome.fi/
  *
  */
 
-/*  Init plugin
+/*
+ *  Load text domain for translations
  *
  */
 
-add_action( 'plugins_loaded', 'CompactChronoInit' );
+add_action( 'plugins_loaded', 'compact_chronological_init' );
 
-function CompactChronoInit( ) {
-	/* Load text domain for i18n */
+function compact_chronological_init( ) {
 	load_plugin_textdomain( 'compact-chronological', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 }
 
 /*
- *  Include default stylesheets
+ *  Include stylesheet for default styling
  *
  */
 
-add_action( 'wp_enqueue_scripts', 'CompactChronoScripts' );
+add_action( 'wp_enqueue_scripts', 'compact_chronological_scripts' );
 
-function CompactChronoScripts( ) {
-	wp_register_style( 'compact-chrono-defaults', plugins_url( 'defaults.css', __FILE__ ) );
-	wp_enqueue_style( 'compact-chrono-defaults' );
+function compact_chronological_scripts( ) {
+	wp_register_style( 'compact-chronological-defaults', plugins_url( 'defaults.css', __FILE__ ) );
+	wp_enqueue_style( 'compact-chronological-defaults' );
 }
 
 
-/*  Widget
+/*
+ *  Register a widget to show the compact monthly view
  *
  */
 
-add_action( 'widgets_init', function( ) { register_widget( 'CompactChronoWidget' ); } );
+add_action( 'widgets_init', function( ) { register_widget( 'compact_chronological_Widget' ); } );
 
-class CompactChronoWidget extends WP_Widget {
-	/** constructor */
-	function __construct() {
-		$widget_ops = array( 'description' => __( 'Show month archive links in a compact view.', 'compact-chronological' ) );
-
-		parent::__construct( 'compact-chronological', _x( 'Compact & Chronological', 'widget name', 'compact-chronological' ), $widget_ops );
+class compact_chronological_Widget extends WP_Widget {
+	public function __construct( ) {
+		parent::__construct(
+			'compact-chronological',
+			_x( 'Compact & Chronological', 'widget name', 'compact-chronological' ),
+			array(
+				'description' => __( 'Monthly archive links in a compact view.', 'compact-chronological' ),
+			)
+		);
 	}
 
 	/** @see WP_Widget::widget */
-	function widget( $args, $instance ) {
+	public function widget( $args, $instance ) {
 		extract( $args );
 		$title = apply_filters( 'widget_title', $instance['title'] );
 
 		echo $before_widget;
 		if( $title ) { echo $before_title . $title . $after_title; }
-		/* */
+
 		global $wpdb, $wp_locale;
 
-		$where = "WHERE post_type = 'post' AND post_status = 'publish'";
-		$query = "SELECT YEAR(post_date) AS `year`, MONTH(post_date) AS `month`, count(ID) as posts FROM $wpdb->posts $join $where GROUP BY YEAR(post_date), MONTH(post_date) ORDER BY YEAR(post_date) DESC, MONTH(post_date) ASC";
+		$query = 'SELECT YEAR(post_date) AS `year`, MONTH(post_date) AS `month`, count(ID) as posts FROM ' . $wpdb->posts . ' WHERE post_type = "post" AND post_status = "publish" GROUP BY YEAR(post_date), MONTH(post_date) ORDER BY YEAR(post_date) DESC, MONTH(post_date) ASC';
 		$archives = $wpdb->get_results( $query );
 
 		if( is_array( $archives ) ) {
-			print '<div class="compact_chrono group">';
+			echo '<div class="compact_chrono group">';
 
 			foreach( $archives as $current ) {
 				$arch[$current->year][$current->month] = $current->posts;
@@ -73,22 +76,22 @@ class CompactChronoWidget extends WP_Widget {
 			$first_year = end( $archives )->year;
 
 			for( $y = $last_year; $y >= $first_year; $y-- ) {
-				if( $instance['split_rows'] == "on" ) {
-					print '<ul class="split">';
+				if( $instance['split_rows'] == 'on' ) {
+					echo '<ul class="split">';
 				} else {
-					print '<ul>';
+					echo '<ul>';
 				}
-				print '<li class="year">';
-				print '<a href="' . get_year_link( $y ) . '" title="' . $y . ": " . sprintf( _n( '%d topic', '%d topics', $ycounts[$y], 'compact-chronological' ), $ycounts[$y] ) . '">';
-				print '<span class="name">' . $y . '</span>';
-				print '</a>';
-				print '</li>'; // intended space!
+				echo '<li class="year">';
+				echo '<a href="' . get_year_link( $y ) . '" title="' . $y . ": " . sprintf( _n( '%d topic', '%d topics', $ycounts[$y], 'compact-chronological' ), $ycounts[$y] ) . '">';
+				echo '<span class="name">' . $y . '</span>';
+				echo '</a>';
+				echo '</li>';
 				for( $m = 1; $m <= 12; $m++ ) {
 					if( $m > date( 'n' ) && $y >= date( 'Y' ) ) { $class = 'month future'; }
 					elseif( $m == date( 'n' ) && $y == date( 'Y' ) ) { $class = 'month now'; }
 					else { $class = 'month past'; }
 
-					print '<li class="' . $class . '">';
+					echo '<li class="' . $class . '">';
 
 					$month_name = ucfirst( strftime( "%B %Y", mktime( 0, 0, 0, $m, 10, $y ) ) );
 					$month_abbr = ucfirst( strftime( "%b", mktime( 0, 0, 0, $m, 10, $y ) ) );
@@ -96,26 +99,25 @@ class CompactChronoWidget extends WP_Widget {
 					if( $arch[$y][$m] < 1 ) {
 						print substr( $month_abbr, 0, 3 );
 					} else {
-						print '<a href="' . get_month_link( $y, $m ) . '" title="' . $month_name . ": " . sprintf( _n( '%d topic', '%d topics', $arch[$y][$m], 'compact-chronological' ), $arch[$y][$m] ) . '">';
-						print '<span class="name">' . substr( $month_abbr, 0, 3 ) . '</span>';
+						echo '<a href="' . get_month_link( $y, $m ) . '" title="' . $month_name . ": " . sprintf( _n( '%d topic', '%d topics', $arch[$y][$m], 'compact-chronological' ), $arch[$y][$m] ) . '">';
+						echo '<span class="name">' . substr( $month_abbr, 0, 3 ) . '</span>';
 						if( $instance['article_counts'] == 1 ) {
-							print '<span class="count">' . $arch[$y][$m] . '</span>';
+							echo '<span class="count">' . $arch[$y][$m] . '</span>';
 						}
-						print '</a>';
+						echo '</a>';
 					}
-					print '</li>';
+					echo '</li>';
 				}
-				print '</ul>';
+				echo '</ul>';
 			}
-
-			print '</div>';
+			echo '</div>';
 		}
 		/* */
 		echo $after_widget;
 	}
 
 	/** @see WP_Widget::update */
-	function update( $new_instance, $old_instance ) {
+	public function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
 		$instance['title'] = strip_tags( $new_instance['title'] );
 		$instance['split_rows'] = $new_instance['split_rows'];
@@ -124,18 +126,15 @@ class CompactChronoWidget extends WP_Widget {
 	}
 
 	/** @see WP_Widget::form */
-	function form( $instance ) {
+	public function form( $instance ) {
 		$title = esc_attr( $instance['title'] );
-
-		if( $instance['split_rows'] == "on" ) { $is_split = "checked=\"checked\""; } else { unset( $is_split ); }
-
 		?>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title', 'compact-chronological' ); ?>
 				<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" />
 			</label>
 			<label for="<?php echo $this->get_field_id( 'split_rows' ); ?>">
-				<input id="<?php echo $this->get_field_id( 'split_rows' ); ?>" name="<?php echo $this->get_field_name( 'split_rows' ); ?>" type="checkbox" value="on" <?php echo $is_split; ?> />
+				<input id="<?php echo $this->get_field_id( 'split_rows' ); ?>" name="<?php echo $this->get_field_name( 'split_rows' ); ?>" type="checkbox" value="on" <?php checked( $instance['split_rows'], 'on' ); ?> />
 				<?php echo _e( 'Split to two rows?', 'compact-chronological' ); ?>
 			</label>
 		</p>
